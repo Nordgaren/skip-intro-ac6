6d2ba840197314c3a84a18b1996e2fff41ebdda2
Nordgaren <nordgarentv@gmail.com>
2023-08-24 19:24:21 -0700

    skip intro lol

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,17 @@
 mod dinput8;
 mod util;
 
+use std::ffi::c_void;
+use std::os::windows::raw::HANDLE;
+use fisherman::scanner::signature::Signature;
+use fisherman::scanner::simple_scanner::SimpleScanner;
+use fisherman::util::get_module_slice;
 use crate::dinput8::init_dinput8;
 use windows::Win32::Foundation::{HMODULE, MAX_PATH};
 #[cfg(feature = "Console")]
 use windows::Win32::System::Console::{AllocConsole, AttachConsole};
-use windows::Win32::System::LibraryLoader::GetModuleFileNameA;
+use windows::Win32::System::LibraryLoader::{GetModuleFileNameA, GetModuleHandleA};
+use windows::Win32::System::Memory::{PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS, VirtualProtect};
 use windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
 
 #[no_mangle]
@@ -22,10 +28,28 @@ pub extern "stdcall" fn DllMain(hinstDLL: isize, dwReason: u32, lpReserved: *mut
                 AttachConsole(u32::MAX);
             }
             init(hinstDLL);
+            init_hooks();
             1
         },
         _ => 0,
     }
+}
+
+unsafe fn init_hooks() {
+    // let base = GetModuleHandleA(None).unwrap().0 as usize;
+    // let module_slice = get_module_slice(base);
+    // let signature = Signature::from_ida_pattern("");
+    // let offset = SimpleScanner
+    //     .scan(module_slice, &signature)
+    //     .expect("Could not find signature.");
+
+    //let callsite = base as isize + offset as isize;
+    let lol = 0x14124eac5 as *mut u16;
+    let mut oldProtect = PAGE_PROTECTION_FLAGS(0);
+    VirtualProtect(lol as *const c_void, 0x2, PAGE_EXECUTE_READWRITE, &mut oldProtect);
+    *lol = 0x9090;
+    VirtualProtect(lol as *const c_void, 0x2, oldProtect, &mut oldProtect);
+
 }
 
 unsafe fn init(hinstDLL: isize) -> String {
